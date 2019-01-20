e90982fa6b53638a10292c21897f73ce049ae137
cosplaygirls <46849713+cosplaygirls@users.noreply.github.com>
2019-01-20 15:53:17 +0300

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
 export const socialsTypes = {
   instagram: 'https://instagram.com/',
   patreon: 'https://patreon.com/',
-  reddit: 'https://reddit.com/r/',
+  reddit: 'https://reddit.com/u/',
   twitter: 'https://twitter.com/',
   facebook: 'https://facebook.com/',
   website: '',
