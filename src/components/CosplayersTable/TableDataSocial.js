f9dabf72680ff6a48d7e932dae823bb4867ec5f8
cosplaygirls <e5pmrDEaVv3f5w@tutanota.com>
2019-01-25 00:01:14 +0300

    Icons

--- src/components/CosplayersTable/TableDataSocial.js
+++ src/components/CosplayersTable/TableDataSocial.js
@@ -1,10 +1,11 @@
 import React, { memo } from 'react';
+import styles from './TableDataSocial.module.scss';
 
-export const TableDataSocial = memo(({ text, url }) => (
-  <td>
+export const TableDataSocial = memo(({ text, url, icon }) => (
+  <td className={styles.center}>
     {text && (
       <a href={url} target="_blank" rel="noopener noreferrer">
-        {text}
+        <img className={styles.icon} src={icon} />
       </a>
     )}
   </td>
--- src/components/CosplayersTable/TableRow.js
+++ src/components/CosplayersTable/TableRow.js
@@ -2,6 +2,14 @@ import React, { memo } from 'react';
 import { TableDataSimple } from './TableDataSimple.js';
 import { TableDataSocial } from './TableDataSocial.js';
 
+import instagramIcon from '../../icons/instagram.svg';
+import patreonIcon from '../../icons/patreon.svg';
+import redditIcon from '../../icons/reddit.svg';
+import twitterIcon from '../../icons/twitter.svg';
+import facebookIcon from '../../icons/facebook.svg';
+import vkIcon from '../../icons/vk.svg';
+import websiteIcon from '../../icons/website.svg';
+
 export const TableRow = memo(({ cosplayer, socialsTypes }) => {
   const {
     origin,
@@ -22,27 +30,43 @@ export const TableRow = memo(({ cosplayer, socialsTypes }) => {
       <TableDataSimple text={nickname} />
       <TableDataSimple text={name} />
       <TableDataSocial
+        icon={instagramIcon}
         text={instagram}
         url={`${socialsTypes.instagram}${instagram}`}
       />
       <TableDataSocial
+        icon={patreonIcon}
         text={patreon}
         url={`${socialsTypes.patreon}${patreon}`}
       />
-      <TableDataSocial text={reddit} url={`${socialsTypes.reddit}${reddit}`} />
       <TableDataSocial
+        icon={redditIcon}
+        text={reddit}
+        url={`${socialsTypes.reddit}${reddit}`}
+      />
+      <TableDataSocial
+        icon={twitterIcon}
         text={twitter}
         url={`${socialsTypes.twitter}${twitter}`}
       />
       <TableDataSocial
+        icon={facebookIcon}
         text={facebook}
         url={`${socialsTypes.facebook}${facebook}`}
       />
-      <TableDataSocial text={vk} url={`${socialsTypes.vk}${vk}`} />
       <TableDataSocial
+        icon={vkIcon}
+        text={vk}
+        url={`${socialsTypes.vk}${vk}`}
+      />
+      <TableDataSocial
+        icon={websiteIcon}
         text={website}
         url={`${socialsTypes.website}${website}`}
       />
     </tr>
   );
 });
+
+// CC0-1.0 https://github.com/simple-icons/simple-icons
+// MIT https://github.com/feathericons/feather
